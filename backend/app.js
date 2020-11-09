7caff99586cb53652c0a232c250d7a679cf3503e
rcorr13 <rachel.corr@unc.edu>
2020-11-08 20:39:15 -0500

    fix links try 1

--- backend/app.js
+++ backend/app.js
@@ -337,12 +337,13 @@ app.put('/updatecontestant/:nameLink', (req, res) => {
             };
         })})
 
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("../build"));
+    app.get('*', (req, res) => res.sendFile(path.resolve('../build', 'index.html')));
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on PORT ${PORT}`);
 });
-
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("../build"));
-}
