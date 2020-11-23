7601e2b74ee36136ffc206768bad3e1c5332280d
rcorr13 <rachel.corr@unc.edu>
2020-11-23 00:06:15 -0500

    Updated how to text

--- src/components/HowTo.js
+++ src/components/HowTo.js
@@ -22,16 +22,23 @@ export default class HowTo extends Component {
                 <TextBelow>
                     <span> &emsp; Before the first episode of the season, each person will sign up for the website and use
                         the <a href="/pickcontestants" >Pick Contestants</a> tool to drag and drop their ideal
-                        contestants onto their team (with contestants higher on the list a higher priority for the
-                        user). Contestants can be on no more than 3 teams and for the first week user picking order
+                        contestants onto their team (with contestants higher on the list being a higher priority for the
+                        user). Contestants can be on no more than 3 teams, and for the first week user picking order
                         is based on results from the prior season's league. After each episode, the scores for each
                         contestant will be calculated according to the <a href="/scoringrules" >Scoring Rules</a> and
                         the <a href="/standings" >Standings</a> will be updated.
                         <br /><br />
                         &emsp; If any of your contestants are eliminated in a given week, you can update your preference
-                        order in the <a href="/pickcontestants" >Pick Contestants</a> tool, and new teams will be assigned
-                        based on your preferences, with users with the lowest current point totals picking first. The
-                        winning user for this season's fantasy league will earn a trophy that I still need to craft.
+                        order in the <a href="/pickcontestants" >Pick Contestants</a> tool (where contestants currently
+                        on your team are highlighted in green), and new teams will be assigned based on your preferences,
+                        with users with the lowest current point totals picking first. The winning user for this
+                        season's fantasy league will hopefully earn a trophy that I still need to craft.
+                        <br /><br />
+                        &emsp; The Bachelorette on ABC this fall is on Tuesdays at 8 PM and all updated teams need to
+                        be posted before then. Additionally, we would like to acknowledge the people at <a
+                            href="https://www.bachbracket.com/">Bach Bracket</a> because some of our rules were
+                        inspired by theirs (although the point values and general design of the leagues are very
+                        different).
                     </span>
                 </TextBelow>
             </div>
