631f687d70fdee263af6bb389e2d2b0cf8846b21
rcorr13 <rachel.corr@unc.edu>
2020-11-22 22:11:49 -0500

    Updated home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
 import RosePic from "../images/HomepageRose.jpg";
 import styled from 'styled-components';
+import {Nav} from "react-bootstrap";
 
 const TextBelow = styled.div`
     font-size: 1em;
-    font-weight: bold;
-    text-align: center;
+    font-weight: normal;
+    text-align: left;
     padding: 8px;
     margin-left: 20px;
-    display: flex;
-    flex-direction: column;
+    display: inline-flex;
     width: 90%;
 `;
 
@@ -19,11 +19,11 @@ export default class Home extends Component {
             <div>
                 <img src={RosePic} alt="rose" style={{width:  "100vw"}}/>
                 <TextBelow>
-                    Yes, there are other Bachelor/Bachelorette fantasy leagues that are well established where you can
+                    &emsp; Yes, there are other Bachelor/Bachelorette fantasy leagues that are well established where you can
                     earn or lose points for contestants on your "team" completing actions like getting a
                     rose or going on a horseback riding date or being shirtless on camera. However, most of
                     these leagues will penalize you if your contestant behaves poorly. We at Final Rose Fantasy
-                    believe that the drama is often the best part of this guilt-pleasure TV show and have created
+                    believe that the drama is often the best part of this guilty-pleasure TV show and have created
                     a new fantasy league that will reward you if your contestant cries (+10) or vomits (+12) or gets
                     a word bleeped out (+7). Contestants on the "most dramatic season ever" deserve to earn points
                     for causing drama, and this fantasy league website allows you to pick your team using our handy
