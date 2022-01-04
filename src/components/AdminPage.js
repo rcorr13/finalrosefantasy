6ee6d4f38f72cd00aa832c09a515c809c386f9b5
rcorr13 <42784467+rcorr13@users.noreply.github.com>
2022-01-04 13:52:35 -0500

    Fix for Bach Season 26

--- src/components/AdminPage.js
+++ src/components/AdminPage.js
@@ -383,8 +383,7 @@ export default class AdminPage extends React.Component {
         (this.state.contestants).forEach(contestant => {
             let weekActions = (contestant[weekActionsColumnName]).map(action => action.key);
             console.log(weekActions);
-            if (weekActions.includes("Does not advance / Eliminated") || weekActions.includes("Leaves for extenuating circumstances (no point penalty)") || weekActions.includes("Leaves of their own accord/not sent home by the 
-            r (unless for emergency reasons)") || weekActions.includes("Production staff sends contestant home")) {
+            if (weekActions.includes("Does not advance / Eliminated") || weekActions.includes("Leaves for extenuating circumstances (no point penalty)") || weekActions.includes("Leaves of their own accord/not sent home by the Bachelor (unless for emergency reasons)") || weekActions.includes("Production staff sends contestant home")) {
                 eliminatedLinks.push(contestant.nameLink);
                 const updatedContestant = {
                     ...contestant,
