1df4eaf28a575b527265546144f1fff17641661f
rcorr13 <rachel.corr@unc.edu>
2022-04-11 12:01:29 -0400

    update Standings to use TableFunctions.js

--- src/components/Standings.js
+++ src/components/Standings.js
@@ -1,47 +1,17 @@
 import React, {useEffect, useState} from "react";
-import PropTypes from "prop-types";
-import { makeStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableContainer from "@material-ui/core/TableContainer";
-import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
-import TableSortLabel from "@material-ui/core/TableSortLabel";
 import Paper from "@material-ui/core/Paper";
 import axios from "axios";
 import GetBaseURL from "./GetBaseURL";
 import {useLocation} from "react-router-dom";
-import withStyles from "@material-ui/core/styles/withStyles";
-import {createStyles} from "@material-ui/styles";
-
-function descendingComparator(a, b, orderBy) {
-    if (b[orderBy] < a[orderBy]) {
-        return -1;
-    }
-    if (b[orderBy] > a[orderBy]) {
-        return 1;
-    }
-    return 0;
-}
-
-function getComparator(order, orderBy) {
-    return order === "desc"
-        ? (a, b) => descendingComparator(a, b, orderBy)
-        : (a, b) => -descendingComparator(a, b, orderBy);
-}
-
-function stableSort(array, comparator) {
-    const stabilizedThis = array.map((el, index) => [el, index]);
-    stabilizedThis.sort((a, b) => {
-        const order = comparator(a[0], b[0]);
-        if (order !== 0) return order;
-        return a[1] - b[1];
-    });
-    return stabilizedThis.map((el) => el[0]);
-}
+import {getComparator, stableSort, useStyles, EnhancedTableHead } from "./TableFunctions"
 
 const headCells = [
+    { id: 'firstname', numeric: false, label: 'Name', minWidth: 100, zIndex: 4},
     { id: 'totalpoints', numeric: true, label: 'Total Points', minWidth: 100 },
     { id: 'week1team', numeric: false, label: 'Week 1 Team', minWidth: 500 },
     { id: 'week1total', numeric: true, label: 'Week 1 Points', minWidth: 200 },
@@ -73,159 +43,9 @@ const headCells = [
     { id: 'week14total', numeric: true, label: 'Week 14 Points', minWidth: 150 },
 ];
 
-const StickyTableCell = withStyles((theme) => ({
-    head: {
-        backgroundColor: "#343a40",
-        left: 0,
-        position: "sticky",
-        zIndex: theme.zIndex.appBar + 2,
-    },
-    body: {
-        minWidth: "50px",
-        left: 0,
-        position: "sticky",
-        zIndex: theme.zIndex.appBar + 1,
-    }
-}))(TableCell);
-
-const StyledTableCell = withStyles((theme) => ({
-    head: {
-        backgroundColor: "#343a40",
-        height: '10px',
-    },
-    body: {
-        fontSize: 14
-    }
-}))(TableCell);
-
-const StyledTableSortLabel = withStyles((theme: Theme) =>
-    createStyles({
-        root: {
-            height: '10px',
-            fontWeight: 'bold',
-            color: 'white',
-            "&:hover": {
-                color: 'lightgrey',
-            },
-            '&$active': {
-                color: 'yellow',
-            },
-        },
-        active: {},
-        icon: {
-            color: 'inherit !important'
-        },
-    })
-)(TableSortLabel);
-
-function EnhancedTableHead(props) {
-    const {classes,order,orderBy,onRequestSort} = props;
-    const createSortHandler = (property) => (event) => {
-        onRequestSort(event, property);
-    };
-    let headCell1 = { id: 'firstname', numeric: false, label: 'Name', minWidth: 100, zIndex: 4};
-return (
-        <TableHead>
-            <TableRow>
-                <StickyTableCell
-                        key={headCell1.id}
-                        //align={headCell.numeric ? "right" : "left"}
-                        //padding={headCell1.disablePadding ? "checkbox" : "default"}
-                        sortDirection={orderBy === headCell1.id ? order : false}
-                        width={300}
-                >
-                        <StyledTableSortLabel
-                            active={orderBy === headCell1.id}
-                            direction={orderBy === headCell1.id ? order : "asc"}
-                            onClick={createSortHandler(headCell1.id)}
-                        >
-                            <p>&nbsp;&nbsp;</p>{headCell1.label}
-                            {orderBy === headCell1.id ? (
-                                <span className={classes.visuallyHidden}>
-                                    {order === "desc" ? "sorted descending" : "sorted ascending"}
-                                </span>
-                            ) : null}
-                        </StyledTableSortLabel>
-                </StickyTableCell>
-                {headCells.map((headCell) => (
-                    <StyledTableCell
-                        key={headCell.id}
-                        //align={headCell.numeric ? "right" : "left"}
-                        padding={headCell.disablePadding ? "none" : "default"}
-                        sortDirection={orderBy === headCell.id ? order : false}
-                        width={300}
-                    >
-                        <StyledTableSortLabel
-                            active={orderBy === headCell.id}
-                            direction={orderBy === headCell.id ? order : "asc"}
-                            onClick={createSortHandler(headCell.id)}
-                        >
-                            {headCell.label}
-                            {orderBy === headCell.id ? (
-                                <span className={classes.visuallyHidden}>
-                                {order === "desc" ? "sorted descending" : "sorted ascending"}
-                                </span>
-                            ) : null}
-                        </StyledTableSortLabel>
-                    </StyledTableCell>
-                ))}
-            </TableRow>
-        </TableHead>
-    );
-}
-
-EnhancedTableHead.propTypes = {
-    classes: PropTypes.object.isRequired,
-    onRequestSort: PropTypes.func.isRequired,
-    order: PropTypes.oneOf(["asc", "desc"]).isRequired,
-    orderBy: PropTypes.string.isRequired,
-};
-
-const useStyles = makeStyles((theme) => ({
-    root: {
-        marginTop: "20px",
-        width: "50%",
-        marginLeft: "25px",
-        marginRight: "25px",
-    },
-    paper: {
-        height: "85vh",
-        width: "95vw",
-        position: 'absolute',
-        overflow: 'scroll',
-    },
-    tableContainer: {
-        overflow: "initial",
-    },
-    cell_long: {
-        width: 200,
-        minWidth: 120,
-    },
-    firstCell: {
-        //position: '-webkit-sticky',
-        position: 'sticky',
-        left: 0,
-        zIndex: 1,
-        backgroundColor: "#343a40",
-        color: "white",
-        width: 200,
-    },
-
-    visuallyHidden: {
-        border: 0,
-        clip: "rect(0 0 0 0)",
-        height: 1,
-        margin: -1,
-        overflow: "hidden",
-        position: "fixed",
-        top: 20,
-        width: 1,
-    },
-}));
-
-
 function removeBachSeason(row) {
     if (typeof row === 'string' || row instanceof String) {
+        console.log(row)
         return row
     } else {
         let newRow = [];
@@ -251,7 +71,9 @@ export default function EnhancedTable() {
                 response.data.forEach(user => {
                     let rowData = (user.picksAndTeams.filter(seasonInfo => seasonInfo.season === season)[0]);
                     let row = Object.assign({firstname: user.firstname}, rowData);
-                    tableData.push(row)
+                    if (Object.keys(row).length > 3) {
+                        tableData.push(row)
+                    }
                 })
                 setUsers(tableData);
             } catch(error) {
@@ -313,6 +135,7 @@ export default function EnhancedTable() {
                             order={order}
                             orderBy={orderBy}
                             onRequestSort={handleRequestSort}
+                            headCells={headCells}
                         />
                         <TableBody>
                             {stableSort(rows, getComparator(order, orderBy)).map(
--- src/components/TableFunctions.js
+++ src/components/TableFunctions.js
@@ -1,10 +1,7 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/core/styles";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import TableSortLabel from "@material-ui/core/TableSortLabel";
