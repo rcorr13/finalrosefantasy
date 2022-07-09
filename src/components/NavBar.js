aae64d9ee8f0e2cff9c487eb409c516f285e67d3
Rachel Corr <42784467+rcorr13@users.noreply.github.com>
2022-07-09 17:37:21 -0400

    Navbar with currentWeek()

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -59,7 +59,7 @@ class NavBar extends Component {
         e.preventDefault();
         this.props.history.push('/admin');
     }
-    
+
     redirectChangePassword(e) {
         e.preventDefault();
         this.props.history.push('/changepassword');
@@ -77,7 +77,7 @@ class NavBar extends Component {
                         <NavDropdown.Item onClick={this.redirectAdmin.bind(this)}>Admin</NavDropdown.Item>
                     )}
                     {(user.id === "5feb78416972daafed8c15c5") && (
-                         <NavDropdown.Item href={"/picks/" + this.state.currentSeason + "/" + this.state.currentWeek}>Picks</NavDropdown.Item>
+                        <NavDropdown.Item href={"/picks/" + this.state.currentSeason() + "/" + this.state.currentWeek()}>Picks</NavDropdown.Item>
                     )}
                 </NavDropdown>
             </Nav>
