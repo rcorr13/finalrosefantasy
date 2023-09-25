203cba2b73d800ab4c2f5e4c948c9afda6f8b0cc
rcorr13 <rcorr@littleotterhealth.com>
2023-09-24 21:20:12 -0600

    undo local changes

--- backend/app.js
+++ backend/app.js
@@ -419,7 +419,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("../build"));
 }
 
-let usingHeroku = false;
+let usingHeroku = true;
 
 if (usingHeroku) {
     app.get('*', (req, res) => res.sendFile(path.resolve('../build', 'index.html')));
